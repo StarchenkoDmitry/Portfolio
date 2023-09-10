4955063eb197bc3141d3665617cd4d57c1e3fc6e
Dimka <starchenkodmitrypavlovich@gmail.com>
2023-09-10 16:03:20 +0300

    add pet in patlist

--- js/listPets.js
+++ js/listPets.js
@@ -5,14 +5,27 @@ const _pet16 = {
     img:"img/pets/pet16.png",
     tags:["ReactJS","ExpressJS","TypeScript","MongoDB","Axios","MobX"],
     url:"",
+    github:"https://github.com/StarchenkoDmitry/pet16_file_sharing_service",
+    description:"В этом проэкте я поработал с базой данных MongoDB ...",
+}
+const _pet3 = {
+    name:"Paint",
+    nameProject:"pet3_Paint",
+    img:"img/pets/pet3.png",
+    tags:["ReactJS","ExpressJS","TypeScript","MongoDB","Axios","MobX"],
+    url:"",
+    github:"https://github.com/StarchenkoDmitry/Pet3_Paint",
+    description:"Это один из моих первых проектов в которых написал простой графический редактор на чистом JavaScript",
+}
+
+const _pet = {
+    name:"",
+    nameProject:"",
+    img:"img/pets/",
+    tags:["ReactJS","ExpressJS","TypeScript","MongoDB","Axios","MobX"],
+    url:"",
     github:"",
     description:"",
 }
-const pets=[_pet16];
 
-const _pet10= {
-    
-}
-
-//test7 main2
-//test8
+const pets=[_pet16,_pet3];
