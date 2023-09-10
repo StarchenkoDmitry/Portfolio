adae82ebbc40c722540ab404851f534047193454
Dimka <starchenkodmitrypavlovich@gmail.com>
2023-09-10 12:55:51 +0300

    new changes

--- js/listPets.js
+++ js/listPets.js
@@ -1,5 +1,17 @@
-const pets=[
-    {
-        "github":""   
-    }
-];
+
+const pets=[_pet16];
+
+const _pet16 = {
+    name:"File Sharing Service",
+    nameProject:"pet16_file_sharing_service",
+    img:"img/pets/pet16.png",
+    tags:["ReactJS","ExpressJS","TypeScript","MongoDB","Axios","MobX"],
+    url:"",
+    github:"",
+    description:"",
+}
+
+const _pet10= {
+
+}
+
